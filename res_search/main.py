72ea27a45e0be7e93abc76396a7e54109da21a0d
david-tompkins-95 <etherealsoftwaresolutions@gmail.com>
2025-01-12 02:46:56 -0600

    Remove

--- res_search/main.py
+++ res_search/main.py
@@ -13,7 +13,7 @@ import os
 from res_search.goog_api.map_interactor import makeReq
 
 def main():
-    key = "AIzaSyBwF8CSFpQUQBtnk7hNNQlx38eFw6836Rk"
+    key = ""
     keyword = "Restaurants"
     radius = "50000"
     lat = 33.1983
