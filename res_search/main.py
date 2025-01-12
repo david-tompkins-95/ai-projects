03949741214881cb72d46d8b83252e07a673cb7a
david-tompkins-95 <etherealsoftwaresolutions@gmail.com>
2025-01-12 01:16:50 -0600

    Build Res Search Repository

---
+++ res_search/main.py
@@ -0,0 +1,14 @@
+### This project is built using Python 3 and the Google Maps API
+# Objective: Find nearby Restaurant Information
+# - Hours
+# - Peak Hours
+# - Phone Number
+# - Address
+# - Food Type
+# - Google Stars
+# Convert Data to simple csv file
+# Built By David M. Tompkins. 
+
+def main():
+    
+    return 0
