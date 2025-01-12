7dbd7a408f23bbbe39bf9b140947e41082aafdb3
david-tompkins-95 <etherealsoftwaresolutions@gmail.com>
2025-01-12 02:44:41 -0600

    Adding some comments

--- res_search/goog_api/map_interactor.py
+++ res_search/goog_api/map_interactor.py
@@ -4,10 +4,12 @@ import os
 def makeReq(tmp_dir, key, keyword, radius, lat, long):
     # Ensure the directory exists
     os.makedirs(tmp_dir, exist_ok=True)
-    print(f"Directory ensured: {tmp_dir}")
+    # Create filepath
     filename_json = "res.json"
     filepath_json = os.path.join(tmp_dir, filename_json)
+    # Make request for google apis
     res = r.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword={}&location={},{}&radius={}&key={}".format(keyword, lat, long, radius, key))
+    # Write JSON to file
     with open(filepath_json, "w", encoding="utf-8") as text_file:
         text_file.write(res.text)
     print("File Written")
