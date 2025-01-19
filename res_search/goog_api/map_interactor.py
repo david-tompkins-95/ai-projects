90943a4f5a2bc4182369c66ae4b9969dc8a47a90
david-tompkins-95 <etherealsoftwaresolutions@gmail.com>
2025-01-19 05:27:19 -0600

    Remove Key, finish code

--- res_search/goog_api/map_interactor.py
+++ res_search/goog_api/map_interactor.py
@@ -4,7 +4,6 @@ import json
 ### Data Needed
 # business_status
 # name
-# opening_hours.open_now
 # price_level
 # rating
 # user_ratings_total
@@ -18,10 +17,27 @@ def makeReq(tmp_dir, key, keyword, radius, lat, long):
     # Make request for google apis
     res = r.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword={}&location={},{}&radius={}&key={}".format(keyword, lat, long, radius, key))
     # Write JSON to file
-    # res = json.loads(res.text)
+    res = json.loads(res.text)
+    if 'results' in res:
+        res = res['results']
+    # Create a list to hold the filtered results
+    filtered_res = []
+
+    # Loop through each object in the list to extract required fields
+    for item in res:
+        print(item)
+        filtered_item = {key: item[key] for key in ['business_status', 'name', 'price_level', 'rating', 'user_ratings_total'] if key in item}
+        filtered_res.append(filtered_item)
+
+    # Convert the filtered list back to JSON
+    res_json = json.dumps(filtered_res, ensure_ascii=False)
+
+    # Writing to the file
     with open(filepath_json, "w", encoding="utf-8") as text_file:
-        text_file.write(res.text)
+        text_file.write(res_json)
+
     print("File Written")
+
 
 def test_speech():
     text = "test"
